9d0d683bbbb82e4666f1b8e899f977f584bd422d
wcafricanus <wcafricanus@gmail.com>
2017-10-23 10:56:04 +0800

    add config.py add another field recording total seconds of a response item's _updated time

--- app/apiserver.py
+++ app/apiserver.py
@@ -1,7 +1,9 @@
+import datetime
 import json
 
 import requests
 from eve import Eve
+
 from app import app
 
 
@@ -10,16 +12,22 @@ def on_insert_testdata_callback(items):
         test = item['test']
         patient_info = test['patientInfo']
         vas_cog_block = test['vasCogBlock']
-        response = requests.post('http://127.0.0.1:5001/mark', json=vas_cog_block)
+        response = requests.post(app.config['MARKER_API_URL'] + 'mark', json=vas_cog_block)
         item['result'] = json.loads(response.text)
         item['patient_id'] = patient_info['patientId']
         item['patient_name'] = patient_info['patientName']
     print(item['result'])
 
 
+def add_timestamp(response):
+    for item in response['_items']:
+        item['_updated_total_seconds'] = (item['_updated'] - datetime.datetime(1970, 1, 1)).total_seconds()
+
+
 class ApiServer(Eve):
     def configure(self):
         self.on_insert_testdata += on_insert_testdata_callback
+        self.on_fetched_resource_testlist += add_timestamp
 
 
 @app.route('/data_migration')
---
+++ config.py
@@ -0,0 +1,1 @@
+MARKER_API_URL = 'http://127.0.0.1:5001/'
